5f0830ea85e0944aab5ac03fc5e302ea654ced84
chaos2007 <chaos2007@gmail.com>
2017-12-16 11:36:48 -0600

    Finish day 10 part 2.

--- day_10/hash.rb
+++ day_10/hash.rb
@@ -25,3 +25,32 @@ def calculate_chain( chain, lengths )
     end
     chain[0] * chain[1]
 end
+
+def convert_ascii_to_byte_array( ascii )
+    byte_array = ascii.chars.map do |element|
+        element.ord
+    end
+end
+
+def calculate_hash( ascii_string ) 
+    chain = (0..255).to_a
+    suffix = [17, 31, 73, 47, 23]
+    lengths = convert_ascii_to_byte_array(ascii_string) + suffix
+    index = 0
+    skip_size = 0
+    (0..63).each do |counter|
+        lengths.each do |length|
+            chain = reverse_segment( chain, length, index )
+            index = (index + skip_size + length) % chain.length
+            skip_size += 1
+        end
+    end
+    checksum_bytes = (0..15).to_a.map do |bucket|
+        "%02x" % create_sparse_hash( chain[bucket*16, 16])
+    end
+    checksum_bytes.reduce("") { |sum, element| sum + element }
+end
+
+def create_sparse_hash( byte_array )
+    byte_array.reduce(0) { |sum, element| sum ^ element }
+end
--- day_10/tc_hash.rb
+++ day_10/tc_hash.rb
@@ -14,4 +14,18 @@ class TestHash < Test::Unit::TestCase
         assert_equal(12, calculate_chain((0..4).to_a, [3,4,1,5]))
         assert_equal(9656, calculate_chain((0..255).to_a, [206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3]))
     end
+
+    def test_converting_ascii_to_byte_array
+        assert_equal( [49,44,50,44,51], convert_ascii_to_byte_array("1,2,3"))
+    end
+
+    def test_calculating_hash
+        assert_equal( "a2582a3a0e66e6e86e3812dcb672a272", calculate_hash(""))
+        assert_equal( "20b7b54c92bf73cf3e5631458a715149", calculate_hash("206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3"))
+
+    end
+
+    def test_creating_hash_from_16_bytes
+        assert_equal( 64, create_sparse_hash([65,27,9,1,4,3,40,50,91,7,6,0,2,5,68,22]))
+    end
 end
