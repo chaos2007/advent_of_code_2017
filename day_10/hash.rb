9730e84dd8132beb9bf73052e0a6167decea5cf3
chaos2007 <chaos2007@gmail.com>
2017-12-16 10:58:06 -0600

    Finished day 10 part 1.

--- day_10/hash.rb
+++ day_10/hash.rb
@@ -1,5 +1,27 @@
 
 def reverse_segment( chain, length, index )
-    chain
+    #case 1.  Where it does not wraparound.
+    if index + length < chain.length
+        prefix = chain[0,index]
+        reverse_portion = chain[index, length].reverse
+        postfix = chain[index+length, chain.length-(index+length)]
+        prefix + reverse_portion + postfix
+    #case 2. Where it does wraparound.
+    else
+        wraparound_amount = (index + length) - chain.length
+        ending_amount = length - wraparound_amount
+        first = chain[index, ending_amount]
+        second = chain[0, wraparound_amount]
+        reverse_portion = (first + second).reverse
+        reverse_portion[ending_amount, reverse_portion.length] + chain[wraparound_amount, index-wraparound_amount] + reverse_portion[0, ending_amount]
+    end
 end
-    
+   
+def calculate_chain( chain, lengths )
+    index = 0
+    lengths.each_with_index do |length, step_size|
+        chain = reverse_segment( chain, length, index )
+        index = (index + step_size + length) % chain.length
+    end
+    chain[0] * chain[1]
+end
--- day_10/tc_hash.rb
+++ day_10/tc_hash.rb
@@ -9,4 +9,9 @@ class TestHash < Test::Unit::TestCase
         assert_equal([4,3,0,1,2], reverse_segment([4,3,0,1,2], 1, 3) )
         assert_equal([3,4,2,1,0], reverse_segment([4,3,0,1,2], 5, 1) )
     end
+
+    def test_solve_chain
+        assert_equal(12, calculate_chain((0..4).to_a, [3,4,1,5]))
+        assert_equal(9656, calculate_chain((0..255).to_a, [206,63,255,131,65,80,238,157,254,24,133,2,16,0,1,3]))
+    end
 end
