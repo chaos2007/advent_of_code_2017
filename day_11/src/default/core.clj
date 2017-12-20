e2a69cf3c515fa5bd9c0bbc0a98f826854ab3814
Don Pollitz <chaos2007@gmail.com>
2017-12-19 21:36:07 -0600

    Add test for figuring out coordinate.

--- day_11/src/default/core.clj
+++ day_11/src/default/core.clj
@@ -6,3 +6,16 @@
   (def x {"n" [0,1], "s" [0,-1], "sw" [-1,-0.5], "nw" [-1,0.5], "se" [1,-0.5], "ne" [1,0.5]}) 
   (x direction)
 )
+
+(defn break_up_directions
+  "Break commas out."
+  [directions]
+  (clojure.string/split directions #",")
+)
+
+(defn figure_out_coordinate
+  "Figure out coordinates."
+  [directions]
+  (def split_directions (break_up_directions directions))
+  (reduce + [0,0] split_directions)
+)
--- day_11/test/default/core_test.clj
+++ day_11/test/default/core_test.clj
@@ -1,6 +1,8 @@
 (ns default.core-test
   (:require [clojure.test :refer :all]
-            [default.core :refer :all]))
+            [default.core :refer :all]
+  )
+)
 
 (deftest tuple-test
   (testing "Getting tuple direction works."
@@ -10,4 +12,20 @@
     (is (= [-1,0.5] (get_direction_tuple "nw")))
     (is (= [1,-0.5] (get_direction_tuple "se")))
     (is (= [1,0.5] (get_direction_tuple "ne")))
-    ))
+  )
+)
+
+(deftest string-split-test
+  (testing "splitting string."
+    (is (= ["nw","n","s"] (break_up_directions "nw,n,s")))
+  )
+)
+
+(deftest figure-out-coordinate
+  (testing "Figure out coordinate"
+    (is (= [3,1.5] (figure_out_coordinate "ne,ne,ne")))
+    (is (= [0,0] (figure_out_coordinate "ne,ne,sw,sw")))
+    (is (= [2,-1] (figure_out_coordinate "ne,ne,s,s")))
+    (is (= [-1,-2.5] (figure_out_coordinate "se,sw,se,sw,sw")))
+  )
+)
